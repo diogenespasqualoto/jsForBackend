a015654633d008b3765973942445435abcf896c9
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-09 22:17:01 -0300

    Tipos primitivos

---
+++ tipos-primitivos/type-boolean.js
@@ -0,0 +1,27 @@
+// const primeiroNumero = 5
+// const segundoNumero = 5;
+// let cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundoNumero)
+
+
+// const texto1 = "Alura"
+// const texto2 = "Alura"
+
+// console.log(texto1 === texto2)
+
+
+let input = null;
+
+if (input === null) {
+    console.log('não há informação');
+} else {
+    console.log(input);
+}
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
