9d5dd40b8bbfb4ec37e4c01141749c782548857d
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-10 19:50:05 -0300

    Arquivos

---
+++ operadores/template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Diógenes";
+const idade = 2023 - 2002;
+const cidadeDeNascimento = "Sarandi-RS";
+
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+// const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+// console.log(apresentacao);
---
+++ operadores/ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeMinima >= idadeCliente) {
+    console.log("Cerveja");
+} else {
+    console.log("suco");
+}
+
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "suco");
+
