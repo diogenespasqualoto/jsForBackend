34c7209a0e4c61a7a38858478e6cac238acbb3ac
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-09 22:17:47 -0300

    Declarando variaveis

---
+++ declarando-variaveis/conversoes.js
@@ -0,0 +1,17 @@
+//  Tipo de dado
+// booleanos
+
+// Conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+
+// Number()
+// String()
+console.log(numero + Number(numeroString));
+console.log(String(numero) + numeroString);
+
+
+// Conversão explícita
+
