2e8820d13a832563491b8c9e5e572165b58304c5
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-11 12:33:49 -0300

    Files

---
+++ js-arrays/o-que-sao-arrays/examples.js
@@ -0,0 +1,26 @@
+/* Exemplos sobre arrays 
+
+const arrayVazia = [,,,];
+console.log(arrayVazia.length)
+console.log(arrayVazia[0])
+console.log(arrayVazia[1])
+console.log(arrayVazia[2])
+
+const arrayVazia = [,,,];
+console.log(arrayVazia.length)
+arrayVazia.push(50)
+console.log(arrayVazia)
+console.log(arrayVazia.length)
+
+*/ 
+
+jantarDeHoje = ['🍔', '🌭', '🍕']
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('🍳')
+jantarDeHoje.push('🥗')
+jantarDeHoje.push('🍏')
+
+console.log(jantarDeHoje)
