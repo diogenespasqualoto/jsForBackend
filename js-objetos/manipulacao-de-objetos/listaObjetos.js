82a4ea1ec5eafae3e0633694b5ef150ba9be9b26
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-12 19:55:51 -0300

    Remember

--- js-objetos/manipulacao-de-objetos/listaObjetos.js
+++ js-objetos/manipulacao-de-objetos/listaObjetos.js
@@ -1,23 +1,27 @@
 const cliente = {
-    nome: "Diógenes",
-    idade: 20,
-    email: "diogenes@pasqualoto.com",
-    telefone: ["1155555550", "1144444440"],
-  };
-  
-  cliente.enderecos = [
-    {
-      rua: "R. Joseph Climber",
-      numero: 1337,
-      apartamento: true,
-      complemento: "ap 934",
-    },
-  ];
-  
-  cliente.enderecos.push({
-    rua: "R. Joseph Ladder",
-    numero: 404,
-    apartamento: false,
-  });
-  
-  console.log(cliente.enderecos);
+  nome: "Diógenes",
+  idade: 20,
+  email: "diogenes@pasqualoto.com",
+  telefone: ["1155555550", "1144444440"],
+};
+
+cliente.enderecos = [
+  {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+  },
+];
+
+cliente.enderecos.push({
+  rua: "R. Joseph Ladder",
+  numero: 404,
+  apartamento: false,
+});
+
+const listaApenasApartamentos = cliente.enderecos.filter(
+  (endereco) => endereco.apartamento === true
+  );
+
+console.log(listaApenasApartamentos);
