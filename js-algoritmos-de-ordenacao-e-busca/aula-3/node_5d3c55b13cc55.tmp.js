1d2d9e17046729415ff2b134aa908694224860bf
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-20 01:11:02 -0300

    Arquivos do modulo quick sort

---
+++ js-algoritmos-de-ordenacao-e-busca/aula-3/node_5d3c55b13cc55.tmp.js
@@ -0,0 +1,26 @@
+const listaLivros = require('./array');
+
+function quickSort(array, esquerda, direita) {
+    if (array.length > 1) {
+        let varTemp = particiona(array, esquerda, direita)
+    }
+    return array;
+}
+
+function particiona(array, esquerda, direita) {
+    let pivo = array[Math.floor((direita) / 2)];
+    let atualEsquerda = esquerda; //0
+    let atualDireita = direita; //10
+
+    while (atualEsquerda < atualDireita) {
+        while (array[atualEsquerda].preco < pivo.preco) {
+            atualEsquerda++
+        }
+        while (array[atualDireita].preco > pivo.preco) {
+            {
+                atualDireita++
+            }
+        }
+    }
+}
+console.log(quickSort(listaLivros, 0, listaLivros.length - 1))
