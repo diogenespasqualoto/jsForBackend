d8906fcd8455f8aee0d531726262494f65de4bed
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-20 23:03:05 -0300

    Arquivos do modulo Usando A Linha de Comando

---
+++ NodeCriandoPrimeiraBiblioteca/usando-a-linha-de-comando/src/index.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+const textoTeste = 'São geralmente recuperados a partir de um objeto[FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => (({ [captura[1]]: captura[2] })));
+    return resultados;
+}
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro, code, 'Não há arquivo no diretório especificado.'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        return extraiLinks(texto);
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+export default pegaArquivo;
