3772a7fa915d1180deb8964dd1635e16df60f47c
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2023-01-25 18:19:24 -0300

    Arquivos do modulo Polimorfismo e SOLID

--- js-programacao-orientada-a-objetos/Admin.js
+++ js-programacao-orientada-a-objetos/Admin.js
@@ -5,6 +5,10 @@ export default class Admin extends User {
     super(nome, email, nascimento, role, ativo)
   }
 
+  exibirInfos() {
+    return `${this.nome},${this.emai}, ${this.role},`
+  }
+
   criarCurso(nomeDoCurso, vagas) {
     return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
   }
